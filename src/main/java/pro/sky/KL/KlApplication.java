b4d86cab273e5bf2ea91f6927a9451a597973418
TenGreen <Grishintut@yandex.ru>
2023-06-07 11:26:40 +0300

    initial commit

---
+++ src/main/java/pro/sky/KL/KlApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.KL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlApplication.class, args);
+	}
+
+}
